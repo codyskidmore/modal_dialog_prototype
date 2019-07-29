53863b9d72b704f91654e1f34af1aef122d4ca37
DESKTOP-4V1NKRS\Cody Skidmore <nospam@skidmore.us>
2019-07-29 17:58:28 -0400

    Changed the header caption.

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 const errorMessageDiv = document.getElementById('modal-container');
-const showErrorDialog = new ShowErrorDialog(errorMessageDiv, 'Modal Header Cody', 'Please fix the following errors and try again.');
+const showErrorDialog = new ShowErrorDialog(errorMessageDiv, 'My Error Dialog', 'Please fix the following errors and try again.');
 const errors = ['Validation Error #1.','Validation Error #2.'];
 showErrorDialog.showErrors(errors);
